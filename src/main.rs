354228ef6216de9fff0c4fdc6f19fe28696a130d
peter bull <17122363+peterbull@users.noreply.github.com>
2025-09-28 14:03:44 -0400

    init commit

---
+++ src/main.rs
@@ -0,0 +1,79 @@
+use rodio::source::{SineWave, Source};
+use std::time::Duration;
+
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
+#[derive(Debug, EnumIter)]
+enum StdScale {
+    C4,
+    CSharp4,
+    D4,
+    EFlat4,
+    E4,
+    F4,
+    FSharp4,
+    G4,
+    GSharp4,
+    A4,
+    BFlat4,
+    B4,
+    C5,
+    CSharp5,
+    D5,
+    EFlat5,
+    E5,
+    F5,
+    FSharp5,
+    G5,
+    GSharp5,
+    A5,
+    BFlat5,
+    B5,
+}
+
+impl StdScale {
+    fn frequency(&self) -> f32 {
+        match self {
+            StdScale::C4 => 261.63,
+            StdScale::CSharp4 => 277.18,
+            StdScale::D4 => 293.66,
+            StdScale::EFlat4 => 311.13,
+            StdScale::E4 => 329.63,
+            StdScale::F4 => 349.23,
+            StdScale::FSharp4 => 369.99,
+            StdScale::G4 => 392.00,
+            StdScale::GSharp4 => 415.30,
+            StdScale::A4 => 440.00,
+            StdScale::BFlat4 => 466.16,
+            StdScale::B4 => 493.88,
+            StdScale::C5 => 523.25,
+            StdScale::CSharp5 => 554.37,
+            StdScale::D5 => 587.33,
+            StdScale::EFlat5 => 622.25,
+            StdScale::E5 => 659.25,
+            StdScale::F5 => 698.46,
+            StdScale::FSharp5 => 739.99,
+            StdScale::G5 => 783.99,
+            StdScale::GSharp5 => 830.61,
+            StdScale::A5 => 880.00,
+            StdScale::BFlat5 => 932.33,
+            StdScale::B5 => 987.77,
+        }
+    }
+}
+
+fn main() {
+    let stream_handle =
+        rodio::OutputStreamBuilder::open_default_stream().expect("open default audio stream");
+    let sink = rodio::Sink::connect_new(&stream_handle.mixer());
+
+    for note in StdScale::iter() {
+        let source = SineWave::new(note.frequency())
+            .take_duration(Duration::from_secs_f32(0.25))
+            .amplify(0.40);
+        sink.append(source);
+    }
+    sink.sleep_until_end(); // sound plays in sep thread and will block main
+    println!("Hello, world!");
+}
